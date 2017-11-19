b768c9dbd4a3947c7621dcaf4e58146b6e5f4d47
Dream-start <32215838+Dream-start@users.noreply.github.com>
2017-11-19 19:59:54 +0800

    Rename WashMachine.java to Count/WashMachine.java

--- Count/WashMachine.java
+++ Count/WashMachine.java
@@ -2,8 +2,8 @@ package com.javatestwork.CarWeight;
 /**
  *<P>Title:</p>
  *<p>Description:</p>
- * @author 幻想乡
- * @2017年11月10日
+ * @author 禄脙脧毛脧莽
+ * @2017脛锚11脭脗10脠脮
  * @WashMachine.java
  */
 public class WashMachine implements ComputeWeight {
